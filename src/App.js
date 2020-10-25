94286155dc63382390ee92aaa9cf953cd864f584
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2020-10-25 20:06:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Checksolution from "./checksolution";
+
+function App() {
+  return (
+    <div className="App">
+      <Checksolution/>
+    </div>
+  );
+}
+
+export default App;
